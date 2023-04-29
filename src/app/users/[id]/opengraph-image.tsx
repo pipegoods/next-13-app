76fc4b4409a986279c4b986f1bdff97b19f6b163
pipegoods <kbarbozab@unicartagena.edu.co>
2023-04-29 12:54:01 -0500

    upda opG

--- src/app/users/[id]/opengraph-image.tsx
+++ src/app/users/[id]/opengraph-image.tsx
@@ -8,29 +8,37 @@ export const alt = 'Imagen de perfil';
 export const contentType = 'image/png';
 
 export default async function og({ params }: { params: { id: number } }) {
-  const user = await getUser(params.id);
+  try {
+    const user = await getUser(params.id);
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 128,
-          background: 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          textAlign: 'center',
-          alignItems: 'center',
-          justifyContent: 'center',
-          gap: 32,
-        }}
-      >
-        <picture>
-          <source srcSet={user.avatar} type="image/webp" />
-          <img src={user.avatar} alt={alt} />
-        </picture>
-        {user.first_name} {user.last_name}
-      </div>
-    )
-  );
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            fontSize: 128,
+            background: 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 32,
+          }}
+        >
+          <picture>
+            <source srcSet={user.avatar} type="image/webp" />
+            <img src={user.avatar} alt={alt} />
+          </picture>
+          {user.first_name} {user.last_name}
+        </div>
+      )
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
